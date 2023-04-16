353a9481f8833143abc14a1690a50a422c6cff33
Quartz <skyu.dev@gmail.com>
2023-04-16 19:36:56 +0900

    feat: add insta logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
 "use client";
 import useInterval from "@/hooks/useInterval";
+import Image from "next/image";
 import React from "react";
 
 export default function Page() {
   const [bannerIndex, setBannerIndex] = React.useState<number>(0);
-  console.log(bannerIndex);
-  useInterval(() => {
-    setBannerIndex((prev) => {
-      console.log("prev", prev);
-      // return prev + 1;
-      return prev < banner.length - 1 ? prev + 1 : 0;
-    });
-  }, 5000);
+  // useInterval(() => {
+  //   setBannerIndex((prev) => {
+  //     return prev < banner.length - 1 ? prev + 1 : 0;
+  //   });
+  // }, 5000);
   return (
     <main id="banner">
       <a href={banner[bannerIndex].url}>
         <div className="flex justify-center w-full p-4">
-          <h1 className="">{banner[bannerIndex].message}</h1>
+          <Image
+            // loader={myLoader}
+            src="/instagram.png"
+            alt="Instagram logo"
+            width={25}
+            height={25}
+          />
+          <span className="ml-1 ">{banner[bannerIndex].message}</span>
         </div>
       </a>
     </main>
@@ -28,12 +33,12 @@ const banner = [
     message: "인스타그램에서 새소식 받기",
     url: "https://www.instagram.com/post_black_belt/",
   },
-  {
-    message: "dd",
-    url: "ddd",
-  },
-  {
-    message: "cc",
-    url: "ddd",
-  },
+  // {
+  //   message: "dd",
+  //   url: "ddd",
+  // },
+  // {
+  //   message: "cc",
+  //   url: "ddd",
+  // },
 ];
