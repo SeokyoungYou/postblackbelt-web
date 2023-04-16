c039df039a1ffdf0f1fbe557b6d5e284667ceb69
Quartz <skyu.dev@gmail.com>
2023-04-16 19:11:36 +0900

    feat: add instagram link

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+import React from "react";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  const [bannerIndex, setBannerIndex] = React.useState(0);
+  return (
+    <main id="banner">
+      <a href={banner[bannerIndex].url}>
+        <div className="flex items-center justify-center w-full h-40">
+          <h1 className="">{banner[bannerIndex].message}</h1>
+        </div>
+      </a>
+    </main>
+  );
 }
+
+const banner = [
+  {
+    message: "인스타그램에서 새소식 받기",
+    url: "https://www.instagram.com/post_black_belt/",
+  },
+  {
+    message: "",
+    url: "",
+  },
+];
