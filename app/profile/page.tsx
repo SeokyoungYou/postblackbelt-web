27aeb8a45dfb7032c621757e93ce2c1684a608af
Quartz <skyu.dev@gmail.com>
2023-04-23 20:12:38 +0900

    feat: add FAQ component

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Button, Space, DatePicker, Card, Avatar } from "antd";
 import Meta from "antd/es/card/Meta";
 import { useSearchParams } from "next/navigation";
+import FAQ from "@/components/FAQ";
 
 const APP_STORE = {
   ios: "https://apps.apple.com/us/app/post-black-belt-%EC%A3%BC%EC%A7%93%EC%88%98-%EB%8B%A4%EC%9D%B4%EC%96%B4%EB%A6%AC-%EC%9D%BC%EC%A7%80/id1673061463",
@@ -18,23 +19,23 @@ export default function ProfilePage() {
   const email = searchParams.get("email");
 
   return (
-    <main className="flex flex-col gap-2 p-4">
+    <main className="flex flex-col gap-4 p-4">
       <a href="https://www.instagram.com/post_black_belt/">
-        <Card>
+        <Card type="inner" title="인스타그램 계정 팔로우하기">
           <Meta
             avatar={<Avatar src="/instagram-filled.png" />}
-            title="인스타그램으로 이동하기"
+            title="인스타그램으로 이동"
             description="@post_black_belt 계정에서 새로운 업데이트 소식을 확인해보세요."
           />
         </Card>
       </a>
-      <Card size="small" title="앱 업데이트하기">
+      <Card type="inner" title="앱 업데이트하기">
         <div className="flex flex-col gap-3">
           <section className="flex items-center justify-between">
             <p>아이폰(iOS)</p>
             <a href={`${APP_STORE.ios}`}>
               <Button type="primary" ghost>
-                앱스토어로 이동하기
+                앱스토어로 이동
               </Button>
             </a>
           </section>
@@ -42,15 +43,25 @@ export default function ProfilePage() {
             <p>갤럭시(Android)</p>
             <a href={`${APP_STORE.android}`}>
               <Button type="primary" ghost>
-                구글 플레이스토어로 이동하기
+                구글 플레이스토어로 이동
               </Button>
             </a>
           </section>
         </div>
       </Card>
-      <a href="https://forms.gle/t3P75wMmDmh2DMzz5">
-        <Card>✏️ 설문조사 참여하여 의견 남기기</Card>
-      </a>
+
+      <Card>
+        <section className="flex items-center justify-between">
+          <p>✏️ 설문조사에 참여하여 의견을 들려주세요! (약 2 분 소요)</p>
+          <a href="https://forms.gle/t3P75wMmDmh2DMzz5">
+            <Button type="primary" ghost>
+              설문조사 하러가기
+            </Button>
+          </a>
+        </section>
+      </Card>
+
+      <FAQ />
     </main>
   );
 }
---
+++ components/FAQ.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { InfoCircleOutlined } from "@ant-design/icons";
+import { Card, Collapse, Typography } from "antd";
+import Title from "antd/es/typography/Title";
+
+const { Panel } = Collapse;
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+function FAQ() {
+  return (
+    <>
+      <Title level={5} style={{ marginTop: "20px", marginBottom: "-5px" }}>
+        FAQ(자주 묻는 질문들)
+      </Title>
+      <Collapse accordion>
+        <Panel header="비밀번호를 잃어버렸어요. 어떻게 찾을 수 있나요?" key="1">
+          <p>
+            보안상의 이유로 비밀번호 찾기는 제공하지 않고 있습니다. 대신 로그인
+            창에서 [비밀번호 초기화하기]를 눌러 가입하신 이메일에서 비밀번호
+            변경이 가능합니다.
+          </p>
+        </Panel>
+        <Panel header="앱을 사용하다 버그를 발견했어요." key="2">
+          <p>
+            인스타그램 DM이나 skyu.dev@gmail.com 으로 연락주시면
+            답변해드리겠습니다.
+          </p>
+        </Panel>
+      </Collapse>
+    </>
+    // </Card>
+  );
+}
+
+export default FAQ;
