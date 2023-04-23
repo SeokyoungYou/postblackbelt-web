6fb66a9273d1cf813986df0ce7213a4d96d5bb8c
Quartz <skyu.dev@gmail.com>
2023-04-23 20:51:07 +0900

    fix: fix use client error

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,4 @@
-import { openGraphImage } from "../shared-metadata";
-export const metadata = {
-  title: "Post Black Belt 주짓수 앱",
-  description: "주짓수 일기 작성을 위한 모바일 앱입니다.",
-  openGraph: {
-    ...openGraphImage,
-    title: "Post Black Belt 주짓수 앱",
-    description: "주짓수 일기 작성을 위한 모바일 앱입니다.",
-  },
-};
-
-("use client");
+"use client";
 import React from "react";
 import { Button, Space, DatePicker, Card, Avatar } from "antd";
 import Meta from "antd/es/card/Meta";
