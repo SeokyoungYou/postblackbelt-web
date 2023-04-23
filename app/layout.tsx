56cd74dd2c094707dabfcd19e7e6eaca5d1e82de
Quartz <skyu.dev@gmail.com>
2023-04-23 18:44:14 +0900

    feat: add gtm script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 // These styles apply to every route in the application
 import "./globals.css";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Script from "next/script";
 
 export const metadata = {
   title: "Post Black Belt",
@@ -16,9 +17,26 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
+          }}
+        ></Script>
         <GoogleAnalytics />
       </head>
       <body>{children}</body>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id='${process.env.GTM_ID}'"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      ></noscript>
     </html>
   );
 }
