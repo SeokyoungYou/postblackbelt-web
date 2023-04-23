6c9ae04a2de02fb6775609a8c8de502c8ed7aba4
Quartz <skyu.dev@gmail.com>
2023-04-23 20:39:36 +0900

    fix: implement og tag directly

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import Script from "next/script";
 export const metadata = {
   title: "Post Black Belt 주짓수 앱",
   description: "주짓수 일기 작성을 위한 모바일 앱입니다.",
-  openGraph: {
-    title: "Post Black Belt 주짓수 앱",
-    description: "주짓수 일기 작성을 위한 모바일 앱입니다.",
-  },
+  // openGraph: {
+  //   title: "Post Black Belt 주짓수 앱",
+  //   description: "주짓수 일기 작성을 위한 모바일 앱입니다.",
+  // },
 };
 
 type RootLayoutProps = {
@@ -22,6 +22,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="ko">
       <head>
         <GoogleAnalytics />
+        <meta
+          property="og:description"
+          content="주짓수 일기 작성을 위한 모바일 앱입니다."
+        ></meta>
+        <meta property="og:title" content="Post Black Belt 주짓수 앱"></meta>
       </head>
       <body>{children}</body>
     </html>
