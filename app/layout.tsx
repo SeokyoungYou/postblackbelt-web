17b78e1ae43d3206cc4d2bbe46b7d55ab5d29088
Quartz <skyu.dev@gmail.com>
2023-04-30 12:31:37 +0900

    add id

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-XXXXXXX');`,
+        })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
           }}
         ></Script>
       </head>
@@ -39,7 +39,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <noscript
           id={process.env.GTM_ID}
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXX"
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         ></noscript>
