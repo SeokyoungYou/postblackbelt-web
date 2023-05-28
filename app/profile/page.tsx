a7dd3d11bb1a9237e2fd9037e9a931a407217e91
Quartz <skyu.dev@gmail.com>
2023-05-28 17:13:44 +0900

    feat: view store button by os

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,12 +4,7 @@ import { Button, Space, DatePicker, Card, Avatar } from "antd";
 import Meta from "antd/es/card/Meta";
 import { useSearchParams } from "next/navigation";
 import FAQ from "@/components/FAQ";
-
-const APP_STORE = {
-  ios: "https://apps.apple.com/us/app/post-black-belt-%EC%A3%BC%EC%A7%93%EC%88%98-%EB%8B%A4%EC%9D%B4%EC%96%B4%EB%A6%AC-%EC%9D%BC%EC%A7%80/id1673061463",
-  android:
-    "https://play.google.com/store/apps/details?id=com.quartz.postblackbelt",
-};
+import { APP_STORE, OS } from "@/lib/constants";
 
 export default function ProfilePage() {
   const searchParams = useSearchParams();
@@ -19,6 +14,30 @@ export default function ProfilePage() {
 
   return (
     <main className="flex flex-col gap-4 p-4">
+      <Card type="inner" title="앱 업데이트하기">
+        <div className="flex flex-col gap-3">
+          {os !== OS.ANDROID && (
+            <section className="flex items-center justify-between">
+              <p>아이폰(iOS)</p>
+              <a href={`${APP_STORE.ios}`} id="appstore-button">
+                <Button type="primary" ghost>
+                  앱스토어 이동
+                </Button>
+              </a>
+            </section>
+          )}
+          {os !== OS.IOS && (
+            <section className="flex items-center justify-between">
+              <p>갤럭시(Android)</p>
+              <a href={`${APP_STORE.android}`} id="playstore-button">
+                <Button type="primary" ghost>
+                  구글 플레이스토어 이동
+                </Button>
+              </a>
+            </section>
+          )}
+        </div>
+      </Card>
       <a
         id="instagram-button"
         href="https://www.instagram.com/post_black_belt/"
@@ -31,26 +50,6 @@ export default function ProfilePage() {
           />
         </Card>
       </a>
-      <Card type="inner" title="앱 업데이트하기">
-        <div className="flex flex-col gap-3">
-          <section className="flex items-center justify-between">
-            <p>아이폰(iOS)</p>
-            <a href={`${APP_STORE.ios}`} id="appstore-button">
-              <Button type="primary" ghost>
-                앱스토어 이동
-              </Button>
-            </a>
-          </section>
-          <section className="flex items-center justify-between">
-            <p>갤럭시(Android)</p>
-            <a href={`${APP_STORE.android}`} id="playstore-button">
-              <Button type="primary" ghost>
-                구글 플레이스토어 이동
-              </Button>
-            </a>
-          </section>
-        </div>
-      </Card>
 
       <Card type="inner" title="✏️ 설문조사 참여하기">
         <section className="flex items-center justify-between">
