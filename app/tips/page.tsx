0c9bd73cac7aac92d3056bf203e6c65cda66ab50
Quartz <skyu.dev@gmail.com>
2023-06-04 21:20:29 +0900

    fix: delete unused variables

--- app/tips/page.tsx
+++ app/tips/page.tsx
@@ -1,19 +1,11 @@
 "use client";
 import React from "react";
-import { Button, Space, DatePicker, Card, Avatar, FloatButton } from "antd";
-import Meta from "antd/es/card/Meta";
 import { useSearchParams } from "next/navigation";
-import FAQ from "@/components/FAQ";
-import { APP_STORE, LATEST_VERSION, OS } from "@/lib/constants";
-import ReleaseNotes from "@/components/ReleaseNotes";
+
 import Image from "next/image";
 
 export default function TipsPage() {
   const searchParams = useSearchParams();
-
-  const os = searchParams.get("os");
-  const email = searchParams.get("email");
-  const version = searchParams.get("version");
 
   return (
     <main className="flex flex-col gap-4 p-4">
